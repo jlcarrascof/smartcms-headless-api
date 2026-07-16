71da68ffde61255dd7b094df1375b31a57bc0cc4
Javier Martinez <entrenamientospuntonet@gmail.com>
2026-07-16 14:14:43 -0400

    Add PostCreateView, PostEditView and register post routes

---
+++ frontend/src/views/posts/PostCreateView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import PostEditor from '@/components/posts/PostEditor.vue'
+
+const router = useRouter()
+
+function onSaved() {
+  router.push('/posts')
+}
+</script>
+
+<template>
+  <PostEditor @saved="onSaved" @cancelled="router.push('/posts')" />
+</template>
---
+++ frontend/src/views/posts/PostEditView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useRoute, useRouter } from 'vue-router'
+import PostEditor from '@/components/posts/PostEditor.vue'
+
+const route = useRoute()
+const router = useRouter()
+const postId = Number(route.params.id)
+
+function onSaved() {
+  router.push('/posts')
+}
+</script>
+
+<template>
+  <PostEditor :post-id="postId" @saved="onSaved" @cancelled="router.push('/posts')" />
+</template>
